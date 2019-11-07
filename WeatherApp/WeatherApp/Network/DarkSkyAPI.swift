120059b84f89e7b253707405e87fba074f044052
Kimball Yang <kimball.yang@gmail.com>
2019-11-06 20:08:50 -0500

    DarkSkyAPI WIP

---
+++ WeatherApp/WeatherApp/Network/DarkSkyAPI.swift
@@ -0,0 +1,30 @@
+//
+//  DarkSkyAPI.swift
+//  WeatherApp
+//
+//  Created by Kimball Yang on 11/6/19.
+//  Copyright © 2019 David Rifkin. All rights reserved.
+//
+
+import Foundation
+
+class DarkSkyAPIManager {
+    
+    private init() {}
+    
+    static let shared = DarkSkyAPIManager()
+    
+    func getForecast(lat: Double, long: Double, completionHandler: @escaping (Result<[WeatherForecast], AppError>) -> Void) {
+        
+        
+        
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+}
